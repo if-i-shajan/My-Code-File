a3ee25f1fa526cd26b438a5d521c047310ac59af
J. M. Ifthakharul Islam Shajan <137435768+if-i-shajan@users.noreply.github.com>
2024-02-14 22:03:46 +0600

    Data Structure
    Data Structure

---
+++ Timer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    int n;
+    printf("Set the Timer:\n");
+    scanf("%d", &n);
+    for (int m = n - 1; m >= 0; m--)
+    {
+        for (int s = 59; s >= 0; s--)
+        {
+            printf("%d : %d\n", m, s);
+            system("color 06");
+            sleep(1);
+            system("cls");
+        }
+    }
+    printf("Time over");
+
+    return 0;
+}
