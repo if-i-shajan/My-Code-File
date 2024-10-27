6e51e5ecd67baf5030725803576fb673174ae1b9
Shajan <jmiishajan2020@gmail.com>
2024-10-28 02:20:40 +0600

    TS-6

---
+++ Game_with_string.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_LEN 100000
+
+int main()
+{
+    char s[MAX_LEN + 1];
+    scanf("%s", s);
+
+    char stack[MAX_LEN];
+    int top = -1;
+    int removalCount = 0;
+
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (top >= 0 && stack[top] == s[i])
+        {
+            top--;
+            removalCount++;
+        }
+        else
+        {
+            stack[++top] = s[i];
+        }
+    }
+
+    if (removalCount % 2 == 1)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+
+    return 0;
+}
+
+// If removalCount is odd, the first player wins, otherwise the second player wins.
