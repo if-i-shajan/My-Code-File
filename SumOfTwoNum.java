48af7327575ed477e24d364e213b9240c02f17b6
J. M. Ifthakharul Islam Shajan <jmiishajan2020@gmail.com>
2025-02-03 17:34:09 +0600

    2.0

---
+++ SumOfTwoNum.java
@@ -0,0 +1,9 @@
+public class SumOfTwoNum {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+}
