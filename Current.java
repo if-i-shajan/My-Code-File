46f8cede8b8154c9b2257aa164a832017e04820b
J. M. Ifthakharul Islam Shajan <jmiishajan2020@gmail.com>
2025-02-18 19:48:55 +0600

    push

--- Current.java
+++ Current.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Current {
     public static void main(String[] args) {
-        int n = 10;
-        System.out.println("Integer value: " + n);
-
+        Scanner input = new Scanner(System.in);
+        int number;
+        System.out.println("Enter ant number: ");
+        number = input.nextInt();
+        System.out.println("Number= " + number);
+        input.close();
     }
 }
